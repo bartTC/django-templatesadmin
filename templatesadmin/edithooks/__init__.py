6a85fbcf91d2ee02bc36d2a0fb03e1ace140e0dd
martin@mahner.org <martin@mahner.org@689794a1-e955-0410-ba0d-ad5301bb33aa>
2008-09-08 11:36:54 +0000

    Templatesadmin v0.4. This is the latest stable version out of my git repository. Now with "edithooks" and "hide readonly files" support.
    git-svn-id: https://django-templatesadmin.googlecode.com/svn/trunk@10 689794a1-e955-0410-ba0d-ad5301bb33aa

---
+++ templatesadmin/edithooks/__init__.py
@@ -0,0 +1,19 @@
+from templatesadmin.forms import TemplateForm
+
+class TemplatesAdminHook(object):
+    '''
+    Hook baseclass
+    '''
+
+    @classmethod
+    def pre_save(cls, request, form, template_path):
+        pass
+
+    @classmethod
+    def post_save(cls, request, form, template_path):
+        pass
+
+    @classmethod
+    def contribute_to_form(cls, form):
+        return form
+
---
+++ templatesadmin/edithooks/gitcommit.py
@@ -0,0 +1,65 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from templatesadmin import TemplatesAdminException
+from templatesadmin.edithooks import TemplatesAdminHook
+
+import subprocess
+import os
+
+class GitCommitHook(TemplatesAdminHook):
+    '''
+    Commit to git after saving
+    '''
+
+    @classmethod
+    def post_save(cls, request, form, template_path):
+        dir, file = os.path.dirname(template_path) + "/", os.path.basename(template_path)
+
+        if request.user.first_name and request.user.last_name:
+            author = "%s %s" % (request.user.first_name, request.user.last_name)
+        else:
+            author = request.user.username
+
+        message = form.cleaned_data['commitmessage'] or '--'
+
+        command = (
+            'GIT_COMMITTER_NAME="%(author)s" GIT_COMMITER_EMAIL="%(email)s" '
+            'GIT_AUTHOR_NAME="%(author)s" GIT_AUTHOR_EMAIL="%(email)s" '
+            'git commit -F - -- %(file)s'
+        ) % {
+          'file': file,
+          'author': author,
+          'email': request.user.email,
+        }
+
+        # Stolen from gitpython's git/cmd.py
+        proc = subprocess.Popen(
+            args=command,
+            shell=True,
+            cwd=dir,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        try:
+            proc.stdin.write(message)
+            proc.stdin.close()
+            stderr_value = proc.stderr.read()
+            stdout_value = proc.stdout.read()
+            status = proc.wait()
+        finally:
+            proc.stderr.close()
+
+        if status != 0:
+            raise TemplatesAdminException("Error while executing %s: %s" % (command, stderr_value.rstrip(), ))
+
+        return stdout_value.rstrip()
+
+    @classmethod
+    def contribute_to_form(cls):
+        return dict(commitmessage=forms.CharField(
+            widget=forms.TextInput(attrs={'size':'100'}),
+            label = _('Change message'),
+            required = False,
+        ))
