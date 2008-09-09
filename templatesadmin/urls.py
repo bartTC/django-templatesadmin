b792daadf4397b246c66da12bcba1d3927c677cc
martin@mahner.org <martin@mahner.org@689794a1-e955-0410-ba0d-ad5301bb33aa>
2008-09-09 10:06:58 +0000

    Applied new directory structure from git repository. Version 0.4-stable
    git-svn-id: https://django-templatesadmin.googlecode.com/svn/trunk@12 689794a1-e955-0410-ba0d-ad5301bb33aa

---
+++ templatesadmin/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url(r'^$', 'templatesadmin.views.overview', name='templatesadmin-overview'),
+    url(r'^edit/(?P<path>.*)/$', 'templatesadmin.views.edit', name='templatesadmin-edit'),
+)
