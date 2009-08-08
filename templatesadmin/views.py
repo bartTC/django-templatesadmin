75108c5498edc6432d698fe64417dea3473f443b
Martin Mahner <martin@mahner.org>
2009-08-08 20:44:25 +0200

    Provide templatedir to templates.

--- templatesadmin/views.py
+++ templatesadmin/views.py
@@ -97,6 +97,7 @@ def listing(request,
                       in TEMPLATESADMIN_VALID_FILE_EXTENSIONS]):
                 full_path = os.path.join(root, f)
                 l = {
++                    'templatedir': templatedir,
                      'rootpath': root,
                      'abspath': full_path,
                      'modified': datetime.fromtimestamp(os.stat(full_path)[ST_MTIME]),
