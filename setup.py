9dfdbf916cbc3f62b68e2b8c0a02daaff684b29e
Martin Mahner <martin@mahner.org>
2009-08-08 21:03:47 +0200

    Fixed overall whitespace

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import os
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
- 
+
 app_name = 'templatesadmin'
 version = __import__(app_name).__version__
- 
+
 # Tell distutils to put the data_files in platform-specific installation
 # locations. See here for an explanation:
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
- 
+
 # Compile the list of packages available, because distutils doesn't have
 # an easy way to do this.
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir:
     os.chdir(root_dir)
- 
+
 for dirpath, dirnames, filenames in os.walk(app_name):
     # Ignore dirnames that start with '.'
     for i, dirname in enumerate(dirnames):
@@ -31,7 +31,7 @@ for dirpath, dirnames, filenames in os.walk(app_name):
         prefix = dirpath[len(app_name)+1:] # Strip "app_name/" or "app_name\"
         for f in filenames:
             data_files.append(os.path.join(prefix, f))
- 
+
 setup(name='django-'+app_name,
       version=version,
       description='A Django app to make minor changes to your templates on the fly.',
